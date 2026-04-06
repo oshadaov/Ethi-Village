70d7f7a6aa420597115cf39d78380d9c0cfc4d4d
oshada <oshadaov@gmail.com>
2026-04-06 18:26:40 +0530

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@
  */
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8085/api/admin";
+  import.meta.env.VITE_API_BASE_URL;
 
 // Generic fetch helper with error handling
 const fetchData = async (endpoint) => {
